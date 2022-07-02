af36da7edf1e58cd8f5bf684412f5114102d5def
zsh <1659583514@qq.com>
2022-07-02 22:04:13 +0800

    router bug fix(menu list)

--- models/menuSchema.js
+++ models/menuSchema.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 const menuSchema = mongoose.Schema({
+  id: String,
   menuType: Number, // 菜单类型
+  children: Array,
+  action: {
+    type: Array,
+    default: [],
+  },
   menuName: String, // 菜单名称
   menuCode: String, // 权限标识
   path: String, // 路由地址
   icon: String, // 图标
   component: String, // 组件地址
   menuState: Number, // 菜单状态
-  parentId: [mongoose.Types.ObjectId],
+  parentId: [String],
   createTime: {
     type: Date,
     default: Date.now(),
